07bcbc4a4aefd90e2d912e92baa79d3eb7260f53
Rizu0007 <87184120+Rizu0007@users.noreply.github.com>
2023-02-22 00:55:49 +0500

    dark mode

---
+++ src/app/Provider.jsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+import { ThemeProvider } from 'next-themes'
+
+
+export default function Provider({children}) {
+  return (
+   <ThemeProvider enableSystem={true}
+   attribute="class">
+   {children}
+   
+   </ThemeProvider>
+  )
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import Header from '@/components/Header'
 import './globals.css'
+import Provider from './Provider'
+
 
 export default function RootLayout({ children }) {
   return (
@@ -10,8 +12,13 @@ export default function RootLayout({ children }) {
       */}
       <head />
       <body>
+      <Provider>
+
       <Header/>
-      {children}</body>
+      {children}
+      
+      </Provider>
+      </body>
     </html>
   )
 }
